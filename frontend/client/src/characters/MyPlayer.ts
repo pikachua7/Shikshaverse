0f07caf906295cad557b1fcc9f7412b9f1497818
Ayush Shete <ayushete2005@gmail.com>
2022-08-18 00:49:12 +0530

    terrace update

--- frontend/client/src/characters/MyPlayer.ts
+++ frontend/client/src/characters/MyPlayer.ts
@@ -63,7 +63,7 @@ export default class MyPlayer extends Player {
           break
         case ItemType.VENDINGMACHINE:
           // hacky and hard-coded, but leaving it as is for now
-          window.open('https://goo.gl/maps/LQHEMm7QPPADYMKD9', '_blank')
+          window.open('https://www.google.com/maps/@28.545409,77.1929044,3a,75y,283.67h,90t/data=!3m7!1e1!3m5!1sPhQVDsPsI3WnljIzP9uRrg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DPhQVDsPsI3WnljIzP9uRrg%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D285.47598%26pitch%3D0%26thumbfov%3D100!7i13312!8i6656', '_blank')
           break
       }
     }
