cb376e49dea5bc6f7321360354f17202367b140b
Ayush Shete <ayushete2005@gmail.com>
2022-08-26 18:05:53 +0530

    Fuck you

--- frontend/client/src/components/header.jsx
+++ frontend/client/src/components/header.jsx
@@ -3,23 +3,24 @@ import {Link} from "react-router-dom"
 export const Header = (props) => {
   return (
     <header id='header'>
-      <div className='intro'>
-        <div className='overlay'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-1 col-md-offset-2 intro-text'>
-                <h1>
-                &nbsp;
-                  <span></span>
-                </h1>
-                <p>&nbsp;</p>
-                <Link to="/map"><img src="https://www.pngrepo.com/png/255181/512/location-pin.png" style={{width:"120px"}}/></Link>
-                
-              </div>
+    <div className='intro'>
+      <div className='overlay'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-1 col-md-offset-2 intro-text'>
+              <h1>
+              &nbsp;
+                <span></span>
+              </h1>
+              <p>&nbsp;</p>
+              <div className="container"style={{width:'200px',color:'white',fontSize:'30px'}}>Start Here</div>
+              <Link to="/map"><img src="https://www.pngrepo.com/png/255181/512/location-pin.png" style={{width:"120px",marginLeft:'40px'}}/></Link>
+              
             </div>
           </div>
         </div>
       </div>
-    </header>
+    </div>
+  </header>
   )
 }
