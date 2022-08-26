2a4caf84c4003c1c716eb05fd9ba2fd39499ce26
Manaswini Verma <85567107+winnie-0-0@users.noreply.github.com>
2022-08-26 16:01:49 +0530

    Fotter delete

--- frontend/client/src/components/Footer.js
+++ frontend/client/src/components/Footer.js
@@ -3,8 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <div className="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-        </div>
+ 
 
 
     </>
