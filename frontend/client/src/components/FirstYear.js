300fb5392ca7a62415a124b27fa1d871f1267012
Ayush Shete <ayushete2005@gmail.com>
2022-08-26 12:07:29 +0530

    Auditorium vala push karra

--- frontend/client/src/components/FirstYear.js
+++ frontend/client/src/components/FirstYear.js
@@ -25,7 +25,13 @@
 // );
 // }
 
+//import First from "./First"
 import React, { useState } from 'react';
+import ten from './img/ten.jpg';
+import audi from './img/audi.jpg';
+import eighteen from './img/eighteen.jpg';
+import comp from './img/comp.jpg';
+import { Pannellum } from "pannellum-react";
 
 const FirstYear = () => {
 
@@ -33,27 +39,31 @@ const FirstYear = () => {
     const tabs = [
         {
             id: 1,
-            tabTitle: 'Tab 1',
-            title: 'Title 1',
-            content: 'Las tabs se generan automáticamente a partir de un array de objetos, el cual tiene las propiedades: id, tabTitle, title y content.'
+            tabTitle: 'Auditorium',
+            title: 'Auditorium',
+            content: 'Las tabs se generan automáticamente a partir de un array de objetos, el cual tiene las propiedades: id, tabTitle, title y content.',
+            image : audi
         },
         {
             id: 2,
-            tabTitle: 'Tab 2',
-            title: 'Title 2',
-            content: 'Contenido de tab 2.'
+            tabTitle: 'Physics Lab',
+            title: 'Physics Lab',
+            content: 'Contenido de tab 2.',
+            image : eighteen
         },
         {
             id: 3,
-            tabTitle: 'Tab 3',
-            title: 'Title 3',
-            content: 'Contenido de tab 3.'
+            tabTitle: 'Chemistry Lab',
+            title: 'Chemistry Lab',
+            content: 'Contenido de tab 3.',
+            image : ten
         },
         {
             id: 4,
-            tabTitle: 'Tab 4',
-            title: 'Title 4',
-            content: 'Contenido de tab 4.'
+            tabTitle: 'Computer Lab',
+            title: 'Computer Lab',
+            content: 'Contenido de tab 4.',
+            image : comp
         }
     ];
 
@@ -62,7 +72,8 @@ const FirstYear = () => {
     }
 
     return (
-        <div className='container'>
+        <div className='container' style={{paddingTop:"2.5%"}}>
+            <iframe class="ku-embed" frameborder="0" allowfullscreen allow="xr-spatial-tracking; gyroscope; accelerometer" scrolling="no" src="https://kuula.co/share/collection/7vzxT?logo=1&info=1&fs=1&vr=0&thumbs=1&inst=0" width={1300} height={800}></iframe>
             <div className='tabs'>
                 {tabs.map((tab, i) =>
                     <button key={i} id={tab.id} disabled={currentTab === `${tab.id}`} onClick={(handleTabClick)}>{tab.tabTitle}</button>
@@ -71,9 +82,31 @@ const FirstYear = () => {
             <div className='content'>
                 {tabs.map((tab, i) =>
                     <div key={i}>
-                        {currentTab === `${tab.id}` && <div><p className='title'>{tab.title}</p><p>{tab.content}</p></div>}
+                        {currentTab === `${tab.id}` && <div><p className='title'>{tab.title}</p><p>{tab.content}</p><div><Pannellum
+        width="100%"
+        height="500px"
+        image={tab.image}
+        pitch={10}
+        yaw={180}
+        hfov={110}
+        autoLoad
+        showZoomCtrl={false}
+        onLoad={() => {
+          console.log("panorama loaded");
+        }}
+      >
+        <Pannellum.Hotspot
+          type="custom"
+          pitch={31}
+          yaw={150}
+          handleClick={(evt, name) => console.log(name)}
+          name="hs1"
+        />
+      </Pannellum></div></div>}
                     </div>
                 )}
+                
+                
             </div>
         </div>
     );
